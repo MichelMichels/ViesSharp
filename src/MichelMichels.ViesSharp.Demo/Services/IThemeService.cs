910f6a975f409f4b3d1889c046776296b05dad67
Michel Michels <michel@miche.ls>
2024-04-30 08:41:23 +0200

    Removed unused project

--- src/MichelMichels.ViesSharp.Demo/Services/IThemeService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MichelMichels.ViesSharp.Demo.Services;
-
-public interface IThemeService
-{
-    event Action? ModeChanged;
-
-    bool IsDarkMode { get; set; }
-    void ToggleMode();
-}
